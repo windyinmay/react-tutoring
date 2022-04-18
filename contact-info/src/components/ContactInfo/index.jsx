60f29ede3a68633877a8dc2f1e526d57e668a5a4
windyinmay <quynhanhmay.butter@gmail.com>
2022-04-18 20:45:36 +0300

    fix errors

--- contact-info/src/components/ContactInfo/index.jsx
+++ contact-info/src/components/ContactInfo/index.jsx
@@ -8,102 +8,39 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
-const columns = [
-    {id: 'firstName', label: 'First Name', minWidth: 70},
-    {id: 'lastName', label: 'Last Name', minWidth: 70},
-    {
-        id: 'phoneNo',
-        label: 'Phone No',
-        minWidth: 70,
-        
-    },
-    {id: 'street', label: 'Street', minWidth: 70},
-    {id: 'postcode', label: 'Post Code', minWidth: 60},
-    {id: 'city', label: 'City', minWidth: 80},
-];
 
-function createData(firstName, lastName, phoneNo, street, postcode, city) {
-    return {firstName, lastName, phoneNo, street, postcode, city};
-}
-
-const contacts =[
-    createData('Vin', 'Fast', '046618760', 'Rakatulina', '00420', 'Helsinki'),
-    createData('Jake', 'Ng', '046619960', 'Latutulina', '00410', 'Vantaa'),
-    createData('Henri', 'Val', '046600960', 'Rakatkala', '00440', 'Espoo'),
-
-]
 export default function ContactInfo(props) {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-   
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-      };
-    
-      const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-      };
-
-    return (
-    <Paper sx={{ width: '100%' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" colSpan={2}>
-                <h3>Name</h3>
+    return(
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>First Name</TableCell>
+            <TableCell>Last Name</TableCell>
+            <TableCell>Phone number</TableCell>
+            <TableCell>Street</TableCell>
+            <TableCell>Postcode</TableCell>
+            <TableCell>City</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.contacts.map((contact) => (
+            <TableRow
+              key={contact.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {contact.firstName}
               </TableCell>
-              <TableCell align="center" colSpan={1}>
-               <h3> Phone</h3>
-              </TableCell>
-              <TableCell align="center" colSpan={3}>
-                <h3>Address</h3>
-              </TableCell>
+              <TableCell>{contact.lastName}</TableCell>
+              <TableCell>{contact.phoneNo}</TableCell>
+              <TableCell>{contact.address.street}</TableCell>
+              <TableCell>{contact.address.postcode}</TableCell>
+              <TableCell>{contact.address.city}</TableCell>
             </TableRow>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ top: 57, minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {contacts
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={contacts.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
--- contact-info/src/views/Main.jsx
+++ contact-info/src/views/Main.jsx
@@ -2,8 +2,43 @@ import React, {useState} from 'react'
 import ContactInfo from '../components/ContactInfo'
 import Navigation from '../components/Nav'
 
+
 export default function Main() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([
+        {
+            firstName: 'Henri',
+            lastName: 'Vadel',
+            phoneNo: '0978322732',
+            address: 
+                {
+                    street: 'Rakatulala',
+                    postcode: '00450',
+                    city: 'Espoo'
+                }
+        },
+        {
+            firstName: 'Jake',
+            lastName: 'Ng',
+            phoneNo: '0978322112',
+            address: 
+                {
+                    street: 'Karatulala',
+                    postcode: '00420',
+                    city: 'Helsinki'
+                }
+        },
+        {
+            firstName: 'Aino',
+            lastName: 'Helda',
+            phoneNo: '0468322732',
+            address: 
+                {
+                    street: 'Kitarakuja',
+                    postcode: '00420',
+                    city: 'Vantaa'
+                }
+        }
+    ]);
     return(
         <div>
         <Navigation setContacts={setContacts} contacts={contacts}/>
