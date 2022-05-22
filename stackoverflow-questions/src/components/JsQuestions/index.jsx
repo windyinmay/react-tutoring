0da299eae3aa3b94cf6eac45669d6e5d4cd1eb1d
windyinmay <quynhanhmay.butter@gmail.com>
2022-05-22 11:30:27 +0300

    generated id and fixing gridData error unique id requested

--- stackoverflow-questions/src/components/JsQuestions/index.jsx
+++ stackoverflow-questions/src/components/JsQuestions/index.jsx
@@ -5,25 +5,47 @@ import axios from 'axios';
 export default function JsQuestions() {
     const [jsQuestion, setJsQuestion] = useState([]);
 
-    const fetchData = async () => await axios.get('https://stackoverflow-api-py.herokuapp.com/api/javascript')
-        .then((res) => res.json())
-        .then(resData => setJsQuestion(resData))
-        .catch(err => console.log(err));
-    
+    const fetchData = async () => {
+        try {
+            const res = await axios.get('https://stackoverflow-api-py.herokuapp.com/api/javascript')
+            return res.data
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    // const fetchData = async () => await axios.get('https://stackoverflow-api-py.herokuapp.com/api/javascript')
+    //     .then((res) => res.json())
+    //     .then(res => {
+    //         console.log(res)
+    //         return res.Data
+    //     })
+    //     .catch(err => console.log(err));
+    const randomizeId = () => Math.floor(Math.random() * 10000)
+
     useEffect(() => async () => {
         const questions = await fetchData();
-        setJsQuestion(questions);
-        console.log(jsQuestion);
+        //manipulate array before call in rows in dataGrid, MUI error asked for unique id
+        const arr = []
+        questions.forEach(q => {
+            const withId = { ...q, id: randomizeId() }
+            arr.push(withId)
+        })
+        setJsQuestion(arr);
+        console.log(arr);
         //ask again about life cycle
-    }, [jsQuestion])
+    }, [])
 
     const columns = [
-        {field: 'questions', headName: 'Questions', width: 120},
-        { field: 'links', headName: 'Links', width: 150 },
-        { field: 'answers', headName: 'Answers', width: 50 },
-        { field: 'votes', headName: 'Votes', width: 50 },
-        { field: 'tags', headName: 'Tags', width: 120 }
+        //{field: 'id', headName: 'Id', width: 50},
+        {field: 'question', headName: 'Questions', width: 300},
+        { field: 'links', headName: 'Links', width: 200 },
+        { field: 'answers', headName: 'Answers', width: 100 },
+        { field: 'votes', headName: 'Votes', width: 100 },
+        { field: 'tags', headName: 'Tags', width: 200 }
     ]
+
+    if (!jsQuestion) return <h1>Loading...</h1> 
         
     return (
         <div>
---
+++ stackoverflow-questions/src/utils/helpers.jsx
@@ -0,0 +1,1 @@
+export const randomizeId = () => Math.floor(Math.random() * 10000)
