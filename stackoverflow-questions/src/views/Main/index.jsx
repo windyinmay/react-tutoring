6bb43bb57443cde2a819baf71c4c07621ec4a565
windyinmay <quynhanhmay.butter@gmail.com>
2022-06-02 02:39:36 +0300

    add router nav

--- stackoverflow-questions/src/views/Main/index.jsx
+++ stackoverflow-questions/src/views/Main/index.jsx
@@ -1,93 +1,39 @@
-import "./packages/react-router-dom/examples/Animation/styles.css";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import JsQuestions from '../../components/JsQuestions';
+import PythonQuestions from '../../components/PythonQuestions';
+import CQuestions from '../../components/CQuestions';
 
-import React from "react";
-import {
-  TransitionGroup,
-  CSSTransition
-} from "react-transition-group";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  useLocation,
-  useParams
-} from "react-router-dom";
-import CQuestions from '../../components/CQuestions'
-import JsQuestions from '../../components/JsQuestions'
-import PythonQuestions from '../../components/PythonQuestions'
 
 export default function Main() {
-   
-    return (
-        <Router>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/main-page" />
-        </Route>
-        <Route path="*">
-          <SwitchViews/>
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
-
-function SwitchViews() {
-    let location = useLocation();
-
-    return (
-        <div>
-            <ul>
-                <NavLink to="/js-questions-from-stack-overflow">Js Questions</NavLink>
-                <NavLink to="/python-questions-from-stack-overflow">Python Questions</NavLink>
-                <NavLink to="/C-questions-from-stack-overflow">C Questions</NavLink>
-            </ul>
-
-            <div>
-                <TransitionGroup>
-                    <CSSTransition
-                        key={location.pathname}
-                        classNames='fade'
-                        timeout={300}
-                    >
-                        <Switch location={location}>
-                            <Route path='/js-questions-from-stack-overflow' children={<JsQuestions />} />
-                            <Route path='/python-questions-from-stack-overflow' children={<PythonQuestions />} />
-                            <Route path='/C-questions-from-stack-overflow' children={<CQuestions />} />
-                        </Switch>
-                    </CSSTransition>
-                </TransitionGroup>
-            </div>
-        </div>
-    );
-};
-
-function NavLink(props) {
-    return (
-      <li>
-        <Link {...props} style={{ color: "inherit" }} />
-      </li>
-    );
-}
-
-
- //implement switch views
- //tried but got errors
-    // const introduction = () => {
-    //         return <p>Introduction</p>
-    //     }
-    // const [selectedTab, setSelectedTab] = useState('');
-    // const SelectedTab = () => {
-    //         switch (selectedTab) {
-    //             case 'JsQuestions':
-    //                 return <JsQuestions />
-    //             case 'PythonQuestions':
-    //                 return <PythonQuestions />
-    //             default:
-    //                 return introduction();
-                    
-    //         }
-    //     }
     
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />} />
+                <Route index element={<Layout />} />
+                <Route path="js-questions" element={<JsQuestions/>} />
+                <Route path="python-questions" element={<PythonQuestions />} />
+                <Route path="C-questions" element={<CQuestions />} />
+            </Routes>
+        </BrowserRouter>
+    );
+    
+    function Layout() {
+        return (
+            <div>
+                <>
+                <h1>Questions from Stack Overflow</h1>
+                <nav>
+                    <Link to='/home'>Home</Link> | {' '}
+                    <Link to='/js-questions'>Js Questions</Link> | {' '}
+                    <Link to='/python-questions'>Python Questions</Link> | {' '}
+                    <Link to='/C-questions'>C Questions</Link>
+                </nav>
+                <Outlet />
+                </>
+                <h3>Let's check the questions</h3>
+            </div>
+        )
+    }
+}
